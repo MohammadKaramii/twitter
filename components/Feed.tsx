a849117bfaad8aa37a0fcc0d9d86c98a53ee623d
MohammadKaramii <karameshqi@gmail.com>
2023-11-07 01:07:24 +0330

    add the post section of the feed component

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,8 +1,30 @@
-import React from 'react'
-import { SparklesIcon } from '@heroicons/react/24/outline'
-import Input from './Input'
+import React from "react";
+import { SparklesIcon } from "@heroicons/react/24/outline";
+import Input from "./Input";
+import Post from "./Post";
 
 const Feed = () => {
+  const posts = [
+    {
+      id: 1,
+      name: "mohammad Karami",
+      username: "mrak",
+      userImg: "https://placehold.co/600x400/png",
+      img: "https://picsum.photos/id/237/600/400",
+      text: "cute dog!",
+      timestamp: "3 hours ago",
+    },
+    {
+      id: 2,
+      name: "mohammad Karami",
+      username: "mrak",
+      userImg: "https://placehold.co/600x400/png",
+      img: "https://picsum.photos/id/238/600/400",
+      text: "city b&w",
+      timestamp: "2 weeks ago",
+    },
+  ];
+
   return (
     <div className="xl:ml-[370px] border-l border-r border-gray-200  xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
       <div className="flex py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
@@ -11,9 +33,12 @@ const Feed = () => {
           <SparklesIcon className="h-5" />
         </div>
       </div>
-        <Input/>
- </div>
-  )
-}
+      <Input />
+      {posts.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+    </div>
+  );
+};
 
-export default Feed
+export default Feed;
