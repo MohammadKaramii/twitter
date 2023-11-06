c2d36c877b5be4191140ba0d03cf86d14804840c
MohammadKaramii <karameshqi@gmail.com>
2023-11-07 00:01:01 +0330

    create feed, its header and input

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Feed from "@/components/Feed";
 import Sidebar  from "@/components/Sidebar";
 
 
@@ -6,6 +7,7 @@ export default function Home() {
     <main className="flex min-h-screen max-w-7xl mx-auto">
 
       <Sidebar/>
+      <Feed />
     </main>
   );
 }
