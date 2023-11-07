72c88a41945672bf3b06bf7324c2b40e5bcb89cf
MohammadKaramii <karameshqi@gmail.com>
2023-11-07 16:56:04 +0330

    create the random users section of the widgets component

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,18 @@ import Feed from "@/components/Feed";
 import Sidebar from "@/components/Sidebar";
 import Widgets from "@/components/Widgets";
 
+export default async function Home({ newsResults, randomUsersResults }: any) {
+  newsResults = await getNews();
+  randomUsersResults = await getRandomUsers();
 
-export  default async function Home({ newsResults }: any) {
-newsResults = await getNews()
- 
-  
   return (
     <main className="flex min-h-screen mx-auto">
       <Sidebar />
       <Feed />
-      <Widgets newsResults={newsResults?.articles} />
-      
+      <Widgets
+        newsResults={newsResults.articles}
+        randomUsersResults={randomUsersResults.results}
+      />
     </main>
   );
 }
@@ -21,14 +22,19 @@ export async function getNews() {
     "https://saurav.tech/NewsAPI/top-headlines/category/technology/us.json",
     { cache: "no-store" }
   );
-  console.log(res);
+
   const newsResults = await res.json();
-  return newsResults
+
+  return newsResults;
 }
 
+export async function getRandomUsers() {
+  const resR = await fetch(
+    "https://randomuser.me/api/?results=30&inc=name,login,picture",
+    { cache: "no-store" }
+  );
 
+  const randomUsersResults = await resR.json();
 
-
-
-// https://saurav.tech/NewsAPI/top-headlines/category/technology/us.json
-
+  return randomUsersResults;
+}
