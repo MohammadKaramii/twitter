dbf25059c7692fba3573ed09422930cac12d9e53
MohammadKaramii <karameshqi@gmail.com>
2023-11-07 04:42:44 +0330

    Create the news section of the widgets component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,20 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
 
-const inter = Inter({ subsets: ['latin'] })
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'Twitter Clone',
-  description: 'Developed by Mohammad karami',
-}
+  title: "Twitter Clone",
+  description: "Developed by Mohammad karami",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,34 @@
 import Feed from "@/components/Feed";
-import Sidebar  from "@/components/Sidebar";
+import Sidebar from "@/components/Sidebar";
 import Widgets from "@/components/Widgets";
 
 
-export default function Home() {
+export  default async function Home({ newsResults }: any) {
+newsResults = await getNews()
+ 
+  
   return (
-    <main className="flex min-h-screen max-w-7xl mx-auto">
-
-      <Sidebar/>
+    <main className="flex min-h-screen mx-auto">
+      <Sidebar />
       <Feed />
-      <Widgets />
+      <Widgets newsResults={newsResults?.articles} />
+      
     </main>
   );
 }
+export async function getNews() {
+  const res = await fetch(
+    "https://saurav.tech/NewsAPI/top-headlines/category/technology/us.json",
+    { cache: "no-store" }
+  );
+  console.log(res);
+  const newsResults = await res.json();
+  return newsResults
+}
+
+
+
+
+
+// https://saurav.tech/NewsAPI/top-headlines/category/technology/us.json
+
